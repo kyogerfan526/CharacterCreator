e204f6a7db906cb0f93a6e35c3dc849e04c1339b
Lane <lanejm1998@gmail.com>
2019-11-13 13:45:40 -0700

    Added Save Button UI Item as well as SaveButton_Click event in .xaml and .xaml.cs

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -114,5 +114,10 @@ namespace CharacterMaker
         {
             // Back Button
         }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
