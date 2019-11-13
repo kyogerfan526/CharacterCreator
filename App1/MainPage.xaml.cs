4318ca2d8076388591d01b4fc125a3c5c38414ed
Lane <lanejm1998@gmail.com>
2019-11-13 14:32:04 -0700

    Added in Full Name functionality; accomplished by editing the Person class. Added slight UI tweaks for better aesthetics.

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -83,6 +83,7 @@ namespace CharacterMaker
         public MainPage()
         {
             this.InitializeComponent();
+            
             pagePerson = new DefaultPerson();
 
             MainGrid.DataContext = pagePerson;
